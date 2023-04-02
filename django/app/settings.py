8e6345a6f2710228ebfbb32b98c3136ba2e3c0ab
rla_thdus <0228ksy@naver.com>
2023-04-02 20:30:31 +0900

    Chore: django secret key 환경 변수 파일로 관리하게 변경

--- django/app/settings.py
+++ django/app/settings.py
@@ -22,7 +22,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t@ovpg+oq##4e7%al*zva+akz_-k9=j-p$jd33%5v$hxoz_++$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
