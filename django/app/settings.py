37bcf6cf6b8624fb6cd11086a234ec12befe941e
rla_thdus <0228ksy@naver.com>
2023-04-04 19:16:43 +0900

    Chore: DEBUG 값 환경 변수 파일로 관리하게 설정

--- django/app/settings.py
+++ django/app/settings.py
@@ -25,7 +25,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
